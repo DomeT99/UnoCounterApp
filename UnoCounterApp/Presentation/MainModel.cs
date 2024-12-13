b3e6320c79a080d1ae6c3549294be269a47bbe21
Domenico Tenace <domenicotenace99@gmail.com>
2024-12-13 22:46:12 +0100

    feat: add decrement logic

--- UnoCounterApp/Presentation/MainModel.cs
+++ UnoCounterApp/Presentation/MainModel.cs
@@ -4,6 +4,7 @@ internal partial record MainModel
 { 
     public IState<Countable> Countable => State.Value(this, () => new Countable(0, 1));
     public ValueTask IncrementCounter() => Countable.UpdateAsync(c => c?.Increment());
+    public ValueTask DecrementCounter() => Countable.UpdateAsync(c => c?.Decrement());
 
 }
 
@@ -12,5 +13,10 @@ internal partial record Countable(int Count, int Step)
     public Countable Increment() => this with
     {
         Count = Count + Step
+    };
+
+    public Countable Decrement() => this with
+    {
+        Count = Count - Step
     };
 }
